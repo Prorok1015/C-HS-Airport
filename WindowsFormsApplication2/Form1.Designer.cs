4d19634a93a8d6e4e7ebf24fa4f64133920f181f
Prorok1015 <45171041+Prorok1015@users.noreply.github.com>
2019-06-04 18:51:34 +0800

    Form1 color edit to aqua

--- WindowsFormsApplication2/Form1.Designer.cs
+++ WindowsFormsApplication2/Form1.Designer.cs
@@ -167,6 +167,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(422, 519);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.panel2);
